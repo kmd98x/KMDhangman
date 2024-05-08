578389945a2eee93b083fb39ec9af6c8a5d0ec14
Martina Doekharan <kmd98x@hotmail.com>
2024-05-08 12:15:07 +0200

    first commit

---
+++ hangmanscript.js
@@ -0,0 +1,27 @@
+const zeroState = document.querySelector (".zero-state") // dit pakt de 'zero-state' uit mijn HMTL
+
+const speelbutton = document.querySelector (".speel") //// dit pakt de 'button' uit mijn HMTL(r.15)
+
+function klikopdebutton () {
+    zeroState.classList.add("hidden") //laat de zero state verdwijnen door een class toe te voegen genaamd 'hidden' en die ga je in css bijwerken (.zero-state.hidden)
+}
+
+speelbutton.addEventListener("click",klikopdebutton) //Als je klikt op de button, dan voert hij de functie uit die daarboven is gedef. (de zero state verdwijnt)
+
+
+
+// ==================================================================================================================================================================================================================================================================
+
+
+const spelRegels = document.querySelector (".spel-regels p") // dit pakt 'spel' uit mijn HTML 
+
+const spelRegelsPijltje = document.querySelector (".spel-regels img")
+
+const spelRegelsButton = document.querySelector (".spel-regels button")
+
+function klikopspelregels () {
+    spelRegelsPijltje.classList.toggle("rotate180")
+    spelRegels.classList.toggle("uitgeklapt")
+}
+
+spelRegelsButton.addEventListener("click", klikopspelregels)
